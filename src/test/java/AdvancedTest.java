20abbfa7528197cf04d47ad1c07ec991a3afc31e
raptorouz <eric.yammine@telecom-st-etienne.fr>
2020-05-22 16:33:32 +0200

    Advanced Tests with roots

--- src/test/java/AdvancedTest.java
+++ src/test/java/AdvancedTest.java
@@ -2,12 +2,68 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
-import org.junit.Test;
-
-import Model.Top3Chains;
+import Model.*;
 import Model.Forest.Country;
 
+import org.junit.Test;
+
 public class AdvancedTest {
+
+	@Test
+	public void test_onlyroots() throws IOException {
+
+		Country countries[] = { Country.SPAIN, Country.FRANCE, Country.ITALY };
+		Country countries1[] = { Country.SPAIN, Country.ITALY, Country.FRANCE };
+		Country countries2[] = { Country.FRANCE, Country.ITALY, Country.SPAIN };
+		Country countries3[] = { Country.FRANCE, Country.SPAIN, Country.ITALY };
+
+		Country countries4[] = { Country.ITALY, Country.FRANCE, Country.SPAIN };
+		Country countries5[] = { Country.ITALY, Country.SPAIN, Country.FRANCE };
+
+		Country[][] countries_ = { countries, countries1, countries2, countries3, countries4, countries5 };
+
+		String path = "resources/data/onlyroots/";
+
+		for (Country tab[] : countries_) {
+			Top3Chains top3 = new Top3Chains(tab, path);
+			top3.debug(true);
+
+			String expected_result = "chainRootId=7, country=SPAIN, score=10\n"
+					+ "chainRootId=12, country=ITALY, score=10\n" + "chainRootId=14, country=FRANCE, score=10";
+			System.out.println(top3.toString());
+			// System.out.println(expected_result);
+
+			assertEquals(expected_result, top3.toString());
+		}
+	}
+	
+	@Test
+	public void test_contaminatedByMultipleRootsWithScore0Chain() throws IOException {
+
+		Country countries[] = { Country.SPAIN, Country.FRANCE, Country.ITALY };
+		Country countries1[] = { Country.SPAIN, Country.ITALY, Country.FRANCE };
+		Country countries2[] = { Country.FRANCE, Country.ITALY, Country.SPAIN };
+		Country countries3[] = { Country.FRANCE, Country.SPAIN, Country.ITALY };
+
+		Country countries4[] = { Country.ITALY, Country.FRANCE, Country.SPAIN };
+		Country countries5[] = { Country.ITALY, Country.SPAIN, Country.FRANCE };
+
+		Country[][] countries_ = { countries, countries1, countries2, countries3, countries4, countries5 };
+
+		String path = "resources/data/contaminatedby0score/";
+
+		for (Country tab[] : countries_) {
+			Top3Chains top3 = new Top3Chains(tab, path);
+			top3.debug(true);
+
+			String expected_result = "chainRootId=7, country=SPAIN, score=10\n"
+					+ "chainRootId=12, country=ITALY, score=10\n" + "chainRootId=17, country=FRANCE, score=10";
+			System.out.println(top3.toString());
+			// System.out.println(expected_result);
+
+			assertEquals(expected_result, top3.toString());
+		}
+	}
 
 	@Test
 	public void fewPeople() throws IOException {
@@ -67,4 +123,7 @@ public class AdvancedTest {
 		
 	}
 
+	
 }
+
+
